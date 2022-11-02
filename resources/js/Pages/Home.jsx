c5cc9b12e336457aa0aee26ce82c4b5c59bacff4
Adit <indrabastari@ymail.com>
2022-11-03 04:06:27 +0700

    update home

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link, Head } from '@inertiajs/inertia-react';
+
+export default function Home(props) {
+    return (
+        <>
+            <Head title="Marketplace Inventory Sync" />
+            hello
+
+        </>
+    );
+}
